681cdb53e8af0940cef1bf5cbabeef10791a7db8
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-29 11:49:37 +0200

    computing total scores for directive and regulation

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,18 +1,36 @@
 class SurveysController < ApplicationController
-  def new
+  def new # GET surveys/new
     @survey = Survey.new
     @survey.answers.build # Required to enable nested attributes
   end
 
-  def create
+  def create # POST surveys
     @survey = Survey.new(survey_params)
-    raise
+    if @survey.save
+      redirect_to survey_path(@survey)
+    else
+      render "survey/show" # <-- Check whether validations errot
+    end
   end
 
-  def show
+  def show # GET surveys/:id
+    @survey = Survey.find(params[:id])
+    @score_directive = compute_scores[:score_directive]
+    @score_regulation = compute_scores[:score_regulation]
   end
 
   private
+
+  def compute_scores
+    scores = { score_directive: 0, score_regulation: 0 }
+    @survey.answers.each do |answer|
+      if answer.compliant == true
+        scores[:score_directive] += answer.weighting_directive
+        scores[:score_regulation] += answer.weighting_regulation
+      end
+    end
+    return scores
+  end
 
   def survey_params
     params.require(:survey).permit(
